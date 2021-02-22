3b600cf92be46b44149dc268ea17a9e1f8619aaf
Richard Lee <richdelee@gmail.com>
2021-02-22 11:15:48 -0700

    Baby steps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button';
 import Banner from './components/Banner.jsx';
 
@@ -7,6 +9,17 @@ class App extends React.Component {
     return (
       <div className="full-app">
         <Banner />
+        <ButtonToolbar aria-label="Toolbar with button groups">
+          <ButtonGroup className="mr-2" aria-label="First group">
+            <Button>1</Button> <Button>2</Button> <Button>3</Button> <Button>4</Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-2" aria-label="Second group">
+            <Button>5</Button> <Button>6</Button> <Button>7</Button>
+          </ButtonGroup>
+          <ButtonGroup aria-label="Third group">
+            <Button>8</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
       </div>
     );
   }
