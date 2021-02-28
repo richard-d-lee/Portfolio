2c4dad95988a54cb3d64728dc5ab87afacb6a106
Richard Lee <richdelee@gmail.com>
2021-02-28 16:07:42 -0700

    new gif!

--- client/src/components/MyCard.jsx
+++ client/src/components/MyCard.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import gif from './gif.gif'
+import gif from './giphy.gif'
 
 function MyCard(props) {
   return (
     <div className="full-card">
       <Card className="my-card" style={{ width: '100%', backgroundColor: "black" }}>
-        <Card.Img variant="top" className="card-image" src={gif} style={{ height: '25rem'}}/>
+        <Card.Img variant="top" className="card-image" src={gif} style={{ width: '25rem', height: 'auto'}}/>
         <Card.Body style={{ backgroundColor: '#333333'}}>
           <center>
             <Button variant="secondary">{props.link}</Button>
