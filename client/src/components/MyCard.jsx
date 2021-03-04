760b7aad9c2bc8db8cf06a4d6fd1af95c8deca30
Richard Lee <richdelee@gmail.com>
2021-03-03 17:54:54 -0700

    Small changes

--- client/src/components/MyCard.jsx
+++ client/src/components/MyCard.jsx
@@ -6,8 +6,8 @@ import gif from './giphy.gif'
 function MyCard(props) {
   return (
     <div className="full-card">
-      <Card className="my-card" style={{ width: '100%', backgroundColor: "black" }}>
-        <Card.Img variant="top" className="card-image" src={gif} style={{ width: '25rem', height: 'auto'}}>
+      <Card className="my-card" style={{ width: '100%', backgroundColor: "black", borderRadius: '15px', border: "1px solid white"}}>
+        <Card.Img variant="top" className="card-image" src={gif} style={{ width: '25rem', height: 'auto', borderRadius: '15px'}}>
           </Card.Img>
           <center>
             <Button className="button" variant="light">{props.link}</Button>
